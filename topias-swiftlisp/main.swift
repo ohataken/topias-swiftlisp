c5120d8fa7808fe83f750f613d024bd075f29d2f
Ken Ohata <ohataken@outlook.com>
2016-11-15 18:36:29 +0900

    Initial Commit

---
+++ topias-swiftlisp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  topias-swiftlisp
+//
+//  Created by Ken Ohata on 2016/11/15.
+//  Copyright © 2016年 Ken Ohata. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
